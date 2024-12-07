cdb7769a1218c8aedd4a35debb0f3a2835e05ade
Mohamad Hanifan <mohamad.hanifan@efishery.com>
2024-12-07 21:31:43 +0700

    feat: move to parent dir

---
+++ leetcode/candy.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+func candy(ratings []int) int {
+
+	candies := make([]int, len(ratings))
+	candies[0] = 1
+
+	for i := range ratings {
+		if i == 0 {
+			continue
+		}
+
+		if ratings[i] > ratings[i-1] {
+			candies[i] = candies[i-1] + 1
+		} else {
+			candies[i] = 1
+
+			for j := i - 1; j >= 0; j-- {
+				if ratings[j] > ratings[j+1] && candies[j] == candies[j+1] {
+					candies[j] = candies[j+1] + 1
+				} else {
+					break
+				}
+			}
+		}
+	}
+
+	sum := 0
+	for _, c := range candies {
+		sum += c
+	}
+
+	fmt.Println("RATINGS", ratings)
+	fmt.Println("CANDIES", candies)
+
+	return sum
+}
--- leetcode/text-justification/go.mod
+++
@@ -1,3 +0,0 @@
-module text-justification
-
-go 1.23.4

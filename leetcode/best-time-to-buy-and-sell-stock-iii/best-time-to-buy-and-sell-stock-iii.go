691fd72495a3e93caff21e0fc6cbb4664f9a1c5a
Mohamad Hanifan <mohamad.hanifan@quipper.com>
2025-06-20 23:03:03 +0700

    best time to sell stock 3

--- leetcode/best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.go
+++ leetcode/best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.go
@@ -14,68 +14,29 @@ func min(a, b int) int {
 	return b
 }
 
-var memMinPrices [][]*int
-var memMaxProfit [][]*int
+func maxProfit(prices []int) int {
 
-func minPrice(prices []int, a int, b int) int {
+	N := len(prices)
 
-	if memMinPrices[a][b] != nil {
-		return *memMinPrices[a][b]
+	maxPro := 0
+
+	buy1 := prices[0]
+	for x := range N {
+		trx1 := prices[x] - buy1
+		buy1 = min(buy1, prices[x])
+		maxPro = max(maxPro, trx1)
+
+		buy2 := 999999
+		for y := x + 1; y < N; y++ {
+
+			trx2 := trx1 + prices[y] - buy2
+			maxPro = max(maxPro, trx2)
+
+			if y > x {
+				buy2 = min(buy2, prices[y])
+			}
+		}
 	}
-
-	res := 0
-	if a == b {
-		res = prices[a]
-	} else {
-		res = min(prices[a], minPrice(prices, a+1, b))
-	}
-	memMinPrices[a][b] = &res
-	return res
-}
-
-func maxProfitKN(prices []int, k, n int) int {
-	if k <= 0 {
-		return 0
-	}
-
-	if n <= 0 {
-		return 0
-	}
-
-	if memMaxProfit[k][n] != nil {
-		return *memMaxProfit[k][n]
-	}
-
-	maxPro := maxProfitKN(prices, k, n-1)
-
-	for j := range k {
-		maxPro = max(maxPro, maxProfitKN(prices, j, n))
-	}
-
-	for m := range n {
-		lastProfit := prices[n] - minPrice(prices, m, n-1)
-		tmpMaxPro := maxProfitKN(prices, k-1, m-1) + lastProfit
-		maxPro = max(maxPro, tmpMaxPro)
-	}
-
-	memMaxProfit[k][n] = &maxPro
 
 	return maxPro
-}
-
-func maxProfit(prices []int) int {
-	k := 2
-	n := len(prices)
-
-	memMinPrices = make([][]*int, n)
-	for i := range n {
-		memMinPrices[i] = make([]*int, n)
-	}
-
-	memMaxProfit = make([][]*int, k+1)
-	for i := range k + 1 {
-		memMaxProfit[i] = make([]*int, n)
-	}
-
-	return maxProfitKN(prices, k, n-1)
 }

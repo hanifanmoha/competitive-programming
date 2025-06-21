bbad2d0f9820f957868af994e6061b8a54174859
Mohamad Hanifan <mohamad.hanifan@quipper.com>
2025-06-21 09:53:17 +0700

    best time to buy stock 3

--- leetcode/best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.go
+++ leetcode/best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.go
@@ -14,6 +14,34 @@ func min(a, b int) int {
 	return b
 }
 
+func maxProfit2(prices []int) int {
+
+	N := len(prices)
+
+	maxPro := 0
+
+	buy1 := prices[0]
+	for x := range N {
+		trx1 := prices[x] - buy1
+		buy1 = min(buy1, prices[x])
+		maxPro = max(maxPro, trx1)
+
+		buy2 := 999999
+
+		for y := x + 1; y < N; y++ {
+
+			trx2 := trx1 + prices[y] - buy2
+			maxPro = max(maxPro, trx2)
+
+			if y > x {
+				buy2 = min(buy2, prices[y])
+			}
+		}
+	}
+
+	return maxPro
+}
+
 func maxProfit(prices []int) int {
 
 	N := len(prices)
